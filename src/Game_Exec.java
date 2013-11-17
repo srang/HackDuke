7f8f2e11e82d007ee7614cb7bbff7af2ca6e6651
Sam Rang <srang2010@gmail.com>
2013-11-16 22:18:47 -0500

    Adding Swing components for main window

--- src/Game_Exec.java
+++ src/Game_Exec.java
@@ -1,4 +1,16 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 public class Game_Exec {
-
+	public static void main(String[] args) { 
+		JFrame frame = new JFrame("Deal Or No Deal");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
